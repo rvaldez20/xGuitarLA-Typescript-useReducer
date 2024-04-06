9b2dfb96f0fb3982cfb75204daade777261afd53
rvaldez20 <rvaldez20@gmail.com>
2024-04-05 19:50:34 -0600

    [ADD] define the reducer cart-reducer.ts and define the CartActions

---
+++ src/reducers/cart-reducers.ts
@@ -0,0 +1,9 @@
+import { Guitar } from "../types";
+
+
+export type CartActivity = 
+   { type: 'add-to-cart', payload: {item: Guitar} } |
+   { type: 'remove-from-cart', payload: {id: Guitar['id']} } |
+   { type: 'decrease-quantity', payload: {id: Guitar['id']} } |
+   { type: 'increase-quantity', payload: {id: Guitar['id']} } |
+   { type: 'clear-cart' } 
